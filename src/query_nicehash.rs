1749661a42d49cc2e2b8e4051a32981856541048
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-10-24 10:46:46 +0100

    refactor

---
+++ src/query_nicehash.rs
@@ -0,0 +1,21 @@
+extern crate reqwest;
+
+fn get_nicehash_response() -> Result<String, self::reqwest::Error>{
+    use std::io::Read;
+    const API_URL: &str = "https://api.nicehash.com/api?method=simplemultialgo.info";
+
+    let mut resp = reqwest::get(API_URL)?;
+    assert!(resp.status().is_success());
+
+    let mut content = String::new();
+    resp.read_to_string(&mut content);
+
+    Ok(content)
+}
+
+#[test]
+fn can_get_nicehash_response(){
+    let response = get_nicehash_response();
+
+    assert!(response.is_ok());
+}
