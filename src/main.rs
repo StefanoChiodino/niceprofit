af093c45043c4778474b57d8fb4765b898fec9f5
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-10-24 10:44:48 +0100

    Add reqwest to nicehash

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,13 @@ use std::process::Command;
 mod runner;
 mod parser;
 mod performance_calculator;
+mod profitability;
 
 const USER_WALLET_ARG: &str = "wallet";
 const DEV_WALLET: &str = "393EZrk5mwZ6gdVYmX5nguesVVJwxD9X2U";
 const CPUMINER_MULTI_PATH: &str = "cpumulti-miner";
 const BENCHMARK_TIME_MS: u64 = 10000;
+const LOCATION: &str = "eu";
 
 fn main() {
     let yaml = load_yaml!("clap.yml");
@@ -30,5 +32,4 @@ fn main() {
         .arg("-p x".to_string());
 
     let output = runner::run(cpuminer_multi_command, BENCHMARK_TIME_MS);
-
 }
