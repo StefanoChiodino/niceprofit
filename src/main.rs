2fb53f137f2a38de5d4d604df7bf3313728ff3a3
Stefano Chiodino <louderth@gmail.com>
2017-10-24 08:33:03 +0100

    constant casing

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,24 @@ use std::process::Command;
 mod runner;
 mod parser;
 
-const user_wallet_arg: &str = "wallet";
-const dev_wallet: &str = "393EZrk5mwZ6gdVYmX5nguesVVJwxD9X2U";
-const cpuminer_multi_path: &str = "cpumulti-miner";
-const benchmark_time_ms: u64 = 10000;
+const USER_WALLET_ARG: &str = "wallet";
+const DEV_WALLET: &str = "393EZrk5mwZ6gdVYmX5nguesVVJwxD9X2U";
+const CPUMINER_MULTI_PATH: &str = "cpumulti-miner";
+const BENCHMARK_TIME_MS: u64 = 10000;
 
 fn main() {
     let yaml = load_yaml!("clap.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let user_wallet = matches.value_of(&user_wallet_arg).unwrap_or(&dev_wallet);
+    let user_wallet = matches.value_of(&USER_WALLET_ARG).unwrap_or(&DEV_WALLET);
 
-    let mut cpuminer_multi_command = Command::new(cpuminer_multi_path);
+    let mut cpuminer_multi_command = Command::new(CPUMINER_MULTI_PATH);
     cpuminer_multi_command
         .arg("-a cryptonight".to_string())
         .arg("-o stratum+tcp://cryptonight.eu.nicehash.com:3355".to_string())
         .arg(format!("-u {}.nicehash-cpumulti-miner-optimiser", user_wallet))
         .arg("-p x".to_string());
 
-    let output = runner::run(cpuminer_multi_command, benchmark_time_ms);
+    let output = runner::run(cpuminer_multi_command, BENCHMARK_TIME_MS);
 
 }
