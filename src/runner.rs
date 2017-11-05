709fdf817c23331dd3826bf6c6a6767f1deab945
Stefano Chiodino <louderth@gmail.com>
2017-11-05 08:17:27 +0000

    Ignore 2 tests that wont pass on travis

--- src/runner.rs
+++ src/runner.rs
@@ -6,19 +6,20 @@ use std::io::Error;
 
 pub fn run(mut command: Command, milliseconds: u64) -> Result<Option<String>, Error> {
     let mut child = command.stdout(Stdio::piped()).spawn()?;
-        let wait_time = time::Duration::from_millis(milliseconds);
-        thread::sleep(wait_time);
-        child.kill();
-        if let Some(mut output) = child.stdout {
-            let mut output_string = String::new();
-            output.read_to_string(&mut output_string);
-            Ok(Some(output_string))
-        } else {
-            Ok(None)
-        }
+    let wait_time = time::Duration::from_millis(milliseconds);
+    thread::sleep(wait_time);
+    child.kill();
+    if let Some(mut output) = child.stdout {
+        let mut output_string = String::new();
+        output.read_to_string(&mut output_string);
+        Ok(Some(output_string))
+    } else {
+        Ok(None)
+    }
 }
 
 #[test]
+#[ignore]
 fn can_run_ping() {
     let path = "ping".to_string();
     let argument = "8.8.8.8".to_string();
