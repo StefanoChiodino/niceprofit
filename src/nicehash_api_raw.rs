b10dcf36a79c0090c2c4289e29ad5e8f4fb1aa3e
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-11-06 22:50:08 +0000

    Revert "Disable ssl for now"
    This reverts commit b382c1f7fa9057e311b58e8b611b6fdbb1490cf0.

--- src/nicehash_api_raw.rs
+++ src/nicehash_api_raw.rs
@@ -4,11 +4,7 @@ pub fn get_nicehash_response() -> Result<String, self::reqwest::Error> {
     use std::io::Read;
     const API_URL: &str = "https://api.nicehash.com/api?method=simplemultialgo.info";
 
-    let mut resp = reqwest::Client::builder()
-        .danger_disable_hostname_verification()
-        .build()?
-        .get(API_URL)
-        .send()?;
+    let mut resp = reqwest::get(API_URL)?;
     assert!(resp.status().is_success());
 
     let mut content = String::new();
