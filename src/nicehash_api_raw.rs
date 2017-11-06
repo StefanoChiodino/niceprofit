dcabbebebe946018710f7539eb6b5e9a8f271478
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-11-06 22:35:34 +0000

    Disable ssl for now

--- src/nicehash_api_raw.rs
+++ src/nicehash_api_raw.rs
@@ -4,7 +4,11 @@ pub fn get_nicehash_response() -> Result<String, self::reqwest::Error> {
     use std::io::Read;
     const API_URL: &str = "https://api.nicehash.com/api?method=simplemultialgo.info";
 
-    let mut resp = reqwest::get(API_URL)?;
+    let mut resp = reqwest::Client::builder()
+        .danger_disable_hostname_verification()
+        .build()?
+        .get(API_URL)
+        .send()?;
     assert!(resp.status().is_success());
 
     let mut content = String::new();
