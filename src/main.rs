c68779381494f611636cb1edbb199684cc4c9541
Stefano Chiodino <i-dont-want@to-be-recuited.thank-you-very-much>
2017-11-07 07:44:54 +0000

    Add env variable for ssl cert to work around https://github.com/japaric/trust/issues/81

--- src/main.rs
+++ src/main.rs
@@ -25,11 +25,15 @@ mod nicehash_cpuminer_mapper;
 mod algorithm_picker;
 mod configuration_provider;
 
+use std::env;
+
 const DEV_WALLET: &str = "393EZrk5mwZ6gdVYmX5nguesVVJwxD9X2U";
 const BENCHMARK_TIME_MS: u64 = 100_000;
 const LOCATION: &str = "eu";
 
 fn main() {
+    // Work around openssl/trust/travis bug https://github.com/japaric/trust/issues/81 .
+    env::set_var("SSL_CERT_DIR", "/etc/ssl/certs");
     let nicehash_response = nicehash_api::get_profitability().unwrap();
     let algorithms= nicehash_response.result.simplemultialgo;
     let benchmark = benchmark::benchmark(&algorithms, LOCATION, BENCHMARK_TIME_MS, &configuration_provider::get_configuration().cpuminer_multi_path, DEV_WALLET);
